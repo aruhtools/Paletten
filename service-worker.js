e6f82342e9ad70386d1e4667231c2e094f83d4de
aruhtools <aruhgmbh@gmail.com>
2026-03-11 11:21:43 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "paletten-pro-firebase-v2";
+const CACHE_NAME = "paletten-pro-firebase-v3";
 const FILES = [
   "./",
   "./index.html",
@@ -11,6 +11,16 @@ self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES))
   );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", (event) => {
